2b02f4e51f521899c48b036dae1861f8d9db0ec5
W1llD <dai.y@live.cn>
2014-11-19 16:05:20 +0800

    Update exdata-008_CP2.R

--- exdata-008_CP2.R
+++ exdata-008_CP2.R
@@ -4,20 +4,12 @@ readRDS("Source_Classification_Code.rds") -> scc
 library(dplyr)
 library(ggplot2)
 
-
 ##plot1
-NEI %>% group_by(year) 
-    %>% summarise(sum(Emissions)) 
-    %>% plot(col="red",type="l")
+NEI %>% group_by(year) %>% summarise(sum(Emissions)) %>% plot(col="red",type="l")
 
 ##plot2
-NEI %>% filter(fips == 24510) 
-    %>% summarise(sum(Emissions)) 
-    %>% plot(col="red",type="l")
+NEI %>% group_by(year) %>% filter(fips == 24510) %>% summarise(sum(Emissions)) %>% plot(col="red",type="l")
 
 ##plot3
+NEI %>% group_by(year,type) %>% filter(fips == 24510) %>% summarise(sum_Emi = sum(Emissions)) %>% ggplot(aes(year, sum_Emi)) + geom_line(colour= "red") + facet_grid(. ~ type)
 
-NEI %>% group_by(year,type) 
-    %>% filter(fips == 24510) 
-    %>% summarise(sum_Emi = sum(Emissions)) 
-    %>% ggplot(aes(year, sum_Emi)) + geom_line(colour= "red") + facet_grid(. ~ type)
