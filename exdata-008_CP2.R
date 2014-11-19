2e9e713497aa4b79f215f6ea44be7f8ae9733372
W1llD <dai.y@live.cn>
2014-11-19 14:01:13 +0800

    Update exdata-008_CP2.R

--- exdata-008_CP2.R
+++ exdata-008_CP2.R
@@ -3,10 +3,10 @@ readRDS("summarySCC_PM25.rds") -> NEI
 readRDS("Source_Classification_Code.rds") -> scc
 
 library(dplyr)
-NEI %>% group_by(year) ->NEI
+NEI %>% group_by(year) -> NEI
 
 ##plot1
-summarise(NEI,sum(Emissions)) %>% plot(col="red",type="l")
+NEI %>% summarise(sum(Emissions)) %>% plot(col="red",type="l")
 
 ##plot2
 NEI %>% filter(fips == 24510) %>% summarise(sum(Emissions)) %>% plot(col="red",type="l")
