708b7876c0e1ad43319771e062071d2150c7e1af
W1llD <dai.y@live.cn>
2014-11-20 10:39:55 +0800

    Update exdata-008_CP2.R

--- exdata-008_CP2.R
+++ exdata-008_CP2.R
@@ -13,3 +13,6 @@ NEI %>% group_by(year) %>% filter(fips == 24510) %>% summarise(sum(Emissions)) %
 ##plot3
 NEI %>% group_by(year,type) %>% filter(fips == 24510) %>% summarise(sum_Emi = sum(Emissions)) %>% ggplot(aes(year, sum_Emi)) + geom_line(colour= "red") + facet_grid(. ~ type)
 
+#plot4
+scc[grep("Coal",scc$EI.Sector),SCC] -> coal
+NEI %>% group_by(year) %>% filter(SCC %in% coal) %>%  summarise(sum_Emi_coal = sum(Emissions)) %>% plot(col="red",type="l")
