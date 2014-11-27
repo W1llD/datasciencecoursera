ecbcc76e7d4d622a17fca20e08429e8096fc333f
W1llD <dai.y@live.cn>
2014-11-27 16:16:05 +0800

    Update exdata-008_CP2.R

--- exdata-008_CP2.R
+++ exdata-008_CP2.R
@@ -3,15 +3,43 @@ readRDS("summarySCC_PM25.rds") -> NEI
 readRDS("Source_Classification_Code.rds") -> scc
 library(dplyr)
 library(ggplot2)
+library(ggthemes)
+
 
 ##plot1
 NEI %>% group_by(year) %>% summarise(sum(Emissions)) %>% plot(col="red",type="l")
 
+####re-write
+NEI %>% group_by(year) %>% summarise(sum(Emissions)) -> p1
+png(file="plot1.png")
+plot(x = p1$Year,
+     y = p1$SumEmission,
+     col = "red",
+     type = "l",
+     xlab = "Year",
+     ylab = "Total of Emissions",
+     sub = "Total emissions from PM2.5 in the United States from 1999 to 2008",
+     xlim = c(1998,2009),
+     ylim = c(1800,3400)) 
+dev.off()
+
 ##plot2
-NEI %>% group_by(year) %>% filter(fips == "24510") %>% summarise(sum(Emissions)) %>% plot(col="red",type="l")
+NEI %>% group_by(year) %>% filter(fips == "24510") %>% summarise(sum(Emissions)) -> p1 
+
+plot(col="red",type="l")
 
 ##plot3
 NEI %>% group_by(year,type) %>% filter(fips == "24510") %>% summarise(sum_Emi = sum(Emissions)) %>% ggplot(aes(year, sum_Emi)) + geom_line(colour= "red") + facet_grid(. ~ type)
+####re-write
+NEI %>% group_by(year,type) %>% filter(fips == "24510") %>% summarise(sum(Emissions)) -> p3
+names(p3) <- c("Year","Type","SumEmissions")
+p <- ggplot(p3, aes(x=Year,  y=SumEmissions, group=Type))
+p <- p + geom_line(size=1, aes(colour=Type, group=Type))
+p <- p + theme_gdocs()
+png(file='plot3.png',width=950)
+p
+dev.off()
+
 
 #plot4
 scc[grep("Coal",scc$EI.Sector),'SCC'] -> coal
